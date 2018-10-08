0ae14bc0cff687ddd0dea43d4b0490087c67a335
Royce Sin <education@roycesin.com>
2018-10-08 15:27:40 -0700

    remove comments and add code book

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,3 @@
-#The submitted data set is tidy.
-#The Github repo contains the required scripts.
-#GitHub contains a code book that modifies and updates the available codebooks with the data to indicate all the variables and summaries calculated, along with units, and any other relevant information.
-#The README that explains the analysis files is clear and understandable.
-#The work submitted for this project is the work of the student who submitted it.
-
 run_analysis <- function(){
     library(dplyr)
     #Import Data
@@ -62,3 +56,7 @@ run_analysis <- function(){
     rm(features_labels,activity_labels)
     rm(tidy_data)
 }
+
+library(dataMaid)
+makeCodebook(filtered_data)
+makeCodebook(avg_data)
